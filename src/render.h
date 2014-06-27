275755c2d46b2b6a5326cfaa318d99972105d288
aarich <arich@hmc.edu>
2014-06-27 14:06:51 +1000

    Fixed tab spacing

--- src/render.h
+++ src/render.h
@@ -23,10 +23,29 @@ using namespace cv;
 namespace render
 {
 
-	#define PT PointXYZRGB
-	Mat makeImagefromSize(PointCloud<PT>::Ptr scloud, const int width, const int height)
-	{
-
+    #define PT PointXYZRGB
+    Mat makeImagefromSize(PointCloud<PT>::Ptr scloud, const int width, const int height)
+    {
+        PointCloud cloud;
+        pcl::fromROSMsg(cloud_transformed, cloud);
+        
+        float resolution = 0.02f;
+        pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> octree (resolution);
+        octree.setInputCloud(cloud.makeShared());
+        octree.addPointsFromInputCloud();
+        
+        if(!((pose->pose.position.x == 0) && (pose->pose.position.y) == 0 && (pose->pose.position.z == 0))) {
+                ROS_WARN("Expecting point to be the origin of its coordinate frame, but it isn't");
+                //TODO broadcast the frame corresponding to this pose?
+        }
+        Eigen::Vector3f    origin(pose->pose.position.x,        pose->pose.position.y, pose->pose.position.z);
+        Eigen::Vector3f direction(pose->pose.position.x + RAND_MAX, pose->pose.position.y + 0.0f, pose->pose.position.z + 0.0f);
+        //AlignedPointTVector voxelCenterList;
+        std::vector<int> k_indices;
+        
+        //int nPoints = octree.getIntersectedVoxelCenters(origin, direction, voxelCenterList);
+        int nPoints = octree.getIntersectedVoxelIndices(origin, direction, k_indices);
+        ROS_INFO("vector intersected %d voxels", nPoints);
 	}
 
 }
