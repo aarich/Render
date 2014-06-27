334721bd37b1c75bdb972ea3beff8b1c090038d7
aarich <arich@hmc.edu>
2014-06-27 14:49:49 +1000

    Fixed file communication issues

---
+++ build/CMakeFiles/render.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/render.dir/src/render.cpp.o"
+  "render.pdb"
+  "render"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/render.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/render_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/render_test.dir/src/main.cpp.o"
+  "render_test.pdb"
+  "render_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/render_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
